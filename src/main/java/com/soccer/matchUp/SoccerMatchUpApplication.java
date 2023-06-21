98b639e5168f7977dab01889ae69eaa6f6c05f73
jaeseonNamgung <sunnamgung8@naver.com>
2023-06-21 09:17:20 +0900

    스프링 초기 설정

---
+++ src/main/java/com/soccer/matchUp/SoccerMatchUpApplication.java
@@ -0,0 +1,13 @@
+package com.soccer.matchUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerMatchUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerMatchUpApplication.class, args);
+	}
+
+}
